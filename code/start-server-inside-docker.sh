190123abcff126ae39c9489b9bc50e5015fbe7d6
jasonlam <jlam@palo-it.com>
2018-11-19 05:01:41 +0800

    move detection to jenkins

--- code/start-server-inside-docker.sh
+++ code/start-server-inside-docker.sh
@@ -1,13 +1,2 @@
 #start-server-inside-docker.sh
-npm start &
-
-pidLength=1
-while [ $pidLength -le 1 ]
-do
-  sleep 1
-  pidLength=$(head -n1 /code/pid.out | wc -c)
-done
-#server started may not ready for response
-sleep 1
-
-#safer now
+npm start
